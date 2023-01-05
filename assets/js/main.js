c5d8fd66b34873de402b621f7c966a05708ee59d
skabuamer <shaikhabuamer@gmail.com>
2023-01-05 23:32:40 +0600

    all page desktop view completed

---
+++ assets/js/main.js
@@ -0,0 +1,55 @@
+var slider = tns({
+    container: ".houseplant-slider",
+    items: 3,
+    mouseDrag: true,
+    nav: false,
+    controls: false,
+    slideBy: 1,
+    gutter: 30,
+});
+
+var slider = tns({
+    container: ".aloe-vera-slider",
+    items: 3,
+    mouseDrag: true,
+    nav: false,
+    controls: false,
+    slideBy: 1,
+    gutter: 30,
+});
+
+var slider = tns({
+    container: ".gardening-slider",
+    items: 3,
+    mouseDrag: true,
+    nav: false,
+    controls: false,
+    slideBy: 1,
+    gutter: 30,
+});
+
+var slider = tns({
+    container: ".nursery-slider",
+    items: 3,
+    mouseDrag: true,
+    nav: false,
+    controls: false,
+    slideBy: 1,
+    gutter: 30,
+});
+
+var slider = tns({
+    container: ".feedbacks-slider",
+    items: 1,
+    axis: "vertical",
+    mouseDrag: true,
+    autoplay: true,
+    autoplayButtonOutput: false,
+    controls: false,
+    navContainer: ".feedback-slider__navs",
+    autoHeight: true,
+});
+
+// jQuery(document).ready(function () {
+//     jQuery("header .offcanvas-body").meanmenu();
+// });

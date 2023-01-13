2fbc9062e0f43495f9b2041dc2237e46bc766cb3
skabuamer <shaikhabuamer@gmail.com>
2023-01-14 01:34:12 +0600

    first complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,55 +1,22 @@
-var slider = tns({
-    container: ".houseplant-slider",
-    items: 3,
-    mouseDrag: true,
-    nav: false,
-    controls: false,
-    slideBy: 1,
-    gutter: 30,
+let lightbox = new FsLightbox(".about-section__video-popup-btn");
+
+window.addEventListener("scroll", () => {
+    // Sticky Header
+    document.querySelector("header").classList.toggle("sticky", window.scrollY >= 100);
 });
 
-var slider = tns({
-    container: ".aloe-vera-slider",
-    items: 3,
-    mouseDrag: true,
-    nav: false,
-    controls: false,
-    slideBy: 1,
-    gutter: 30,
+/*--------- BACK TO TOP START ---------*/
+window.addEventListener("scroll", () => {
+    document.querySelector(".back__to__top").classList.toggle("active", scrollY > 300);
+
+    if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
+        document.querySelector(".back__to__top").style.transform = `translateY(-${document.querySelector(".footer-bottom").clientHeight - 20}px)`;
+    } else {
+        document.querySelector(".back__to__top").style.transform = "translateY(0px)";
+    }
 });
 
-var slider = tns({
-    container: ".gardening-slider",
-    items: 3,
-    mouseDrag: true,
-    nav: false,
-    controls: false,
-    slideBy: 1,
-    gutter: 30,
+document.querySelector(".back__to__top").addEventListener("click", () => {
+    window.scrollTo(0, 0);
 });
-
-var slider = tns({
-    container: ".nursery-slider",
-    items: 3,
-    mouseDrag: true,
-    nav: false,
-    controls: false,
-    slideBy: 1,
-    gutter: 30,
-});
-
-var slider = tns({
-    container: ".feedbacks-slider",
-    items: 1,
-    axis: "vertical",
-    mouseDrag: true,
-    autoplay: true,
-    autoplayButtonOutput: false,
-    controls: false,
-    navContainer: ".feedback-slider__navs",
-    autoHeight: true,
-});
-
-// jQuery(document).ready(function () {
-//     jQuery("header .offcanvas-body").meanmenu();
-// });
+/*--------- BACK TO TOP END ---------*/
